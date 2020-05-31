e1f4168ea4835316790196337641330646a92bf9
Your Name <damiangarz@gmail.com>
2020-05-30 20:43:19 -0500

    Improving moar

--- src/main/java/pages/locators/GotoKartLocators.java
+++ src/main/java/pages/locators/GotoKartLocators.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class GotoKartLocators {
-    //@FindBy(how = How.ID, using = "hlb-view-cart-announce") //ID Selector failed at some executions, replated for Link Text
+    //@FindBy(how = How.ID, using = "hlb-view-cart-announce") //ID Selector failed at some executions, replaced for Link Text
     @FindBy(how = How.LINK_TEXT, using = "Carrito")
     public WebElement goToKartBtn;
 }
